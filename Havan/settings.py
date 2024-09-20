102601e366b8fc6641f7caa819b4188e62fa1375
larissa de guimaraes gomes <larissaggomes.com>
2024-09-20 19:16:50 -0400

    feat: adicinado database postgres

--- Havan/settings.py
+++ Havan/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Havan.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vendas',
+        'HOST': 'localhost',
+        'PORT': 5432,
+        'USER': 'postgres',
+        'PASSWORD': '123456',
     }
 }
 
