130356f1669787263ca8c1c4649d4b98462683fb
larissa de guimaraes gomes <larissaggomes.com>
2024-09-20 21:19:13 -0400

    feat: adicionado tabelas

--- sale/models.py
+++ sale/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
 # Create your models here.
+class ModelBase(models.Model):
+    id = models.AutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        managed = True
+        abstract = True
+
+
+class Department(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+
+    class Meta:
+        db_table = 'department'
+
+
+class MaritalStatus(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+
+    class Meta:
+        db_table = 'marital_status'
+
+
+class State(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    abbreviation = models.CharField(max_length=2, null=False)
+
+    class Meta:
+        db_table = 'state'
+
+
+class City(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    state = models.ForeignKey(
+        to='State',
+        on_delete=models.DO_NOTHING(),
+        null=False,
+    )
+
+    class Meta:
+        db_table = 'city'
+
+
+class Employee(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    department = models.ForeignKey(
+        to='Department',
+        on_delete=models.DO_NOTHING(),
+        null=False,
+    )
+
+    class meta:
+        db_table = 'employee'
