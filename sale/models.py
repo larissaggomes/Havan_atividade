14614b72c50c75ca2fa74ab24ec1e4903a00e538
larissa de guimaraes gomes <larissaggomes.com>
2024-09-23 19:37:24 -0400

    feat: adicionado district e outras tabelas

--- sale/models.py
+++ sale/models.py
@@ -85,3 +85,118 @@ class Customer(ModelBase):
 
     class Meta:
         db_table = 'customer'
+
+
+class District(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    zone = models.ForeignKey(
+        to='Zone',
+        on_delete=models.DO_NOTHING,
+        db_column='id_zone',
+        null=False,
+    )
+    city = models.ForeignKey(
+        to='City',
+        on_delete=models.DO_NOTHING,
+        db_column='id_city',
+        null=False,
+    )
+
+    class Meta:
+        db_table = 'district'
+
+
+class Branch(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    district = models.ForeignKey(
+        to='District',
+        on_delete=models.DO_NOTHING,
+        db_column='id_district',
+        null=False,
+    )
+
+    class Meta:
+        db_table = 'brach'
+
+
+class Product(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    cost_price = models.DecimalField(max_digits=16, decimal_places=2, null=False)
+    sale_price = models.DecimalField(max_digits=16, decimal_places=2, null=False)
+    product_group = models.ForeignKey(
+        to='ProductGroup',
+        on_delete=models.DO_NOTHING,
+        db_column='id_product_group',
+        null=False,
+    )
+    supplier = models.ForeignKey(
+        to='Supplier',
+        on_delete=models.DO_NOTHING,
+        db_column='id_supplier',
+        null=False,
+    )
+
+    class Meta:
+        db_table = 'product'
+
+
+class ProductGroup(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    comission_percent = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+    gain_percent = models.DecimalField(max_digits=5, decimal_places=2, null=False)
+
+    class Meta:
+        db_table = 'product_group'
+
+
+class Supplier(ModelBase):
+    name = models.CharField(max_length=64, null=False)
+    legal_document = models.CharField(max_length=20, null=False)
+
+    class Meta:
+        db_table = 'supplier'
+
+
+class Sale(ModelBase):
+    date = models.DateField(null=False)
+    customer = models.ForeignKey(
+        to='Customer',
+        on_delete=models.DO_NOTHING,
+        db_column='id_customer',
+        null=False,
+    )
+    branch = models.ForeignKey(
+        to='Branch',
+        on_delete=models.DO_NOTHING,
+        db_column='id_branch',
+        null=False,
+    )
+    employee = models.ForeignKey(
+        to='Employee',
+        on_delete=models.DO_NOTHING,
+        db_column='id_employee',
+        null=False,
+    )
+
+    class Meta:
+        db_table = 'sale'
+
+
+class SaleProduct(ModelBase):
+    quantity = models.DecimalField(max_digits=16, decimal_places=2, null=False)
+    sale_price = models.DecimalField(max_digits=16, decimal_places=2, null=False)
+    sale = models.ForeignKey(
+        to='Sale',
+        on_delete=models.DO_NOTHING,
+        db_column='id_sale',
+        null=False,
+    )
+    product = models.ForeignKey(
+        to='Product',
+        on_delete=models.DO_NOTHING,
+        db_column='id_product',
+        null=False,
+    )
+
+    class Meta:
+        db_table = 'sale_product'
