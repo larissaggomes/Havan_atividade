6e021bb57493f51fdaf8f5149e14f3c3e6e65ef1
larissa de guimaraes gomes <larissaggomes.com>
2024-09-23 20:54:42 -0400

    feat: adicionado maritalstatus e outros

--- sale/admin.py
+++ sale/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from sale.models import MaritalStatus, State, City
+
 
 # Register your models here.
+@admin.register(MaritalStatus)
+class MaritalStatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'modified_at', 'active']
+    list_display_links = ['id', 'name', 'created_at', 'modified_at', 'active']
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'modified_at', 'active']
+    list_display_links = ['id', 'name', 'created_at', 'modified_at', 'active']
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'created_at', 'modified_at', 'active']
+    list_display_links = ['id', 'name', 'created_at', 'modified_at', 'active']
+
+
+@admin.register(MaritalStatus)
